b4779059b127285a047e65af8d03eda7b61cc04d
Nikolas Laus <bchik3@gmail.com>
2021-02-21 18:34:18 +0200

    Code style.

--- src/components/Warrior.vue
+++ src/components/Warrior.vue
@@ -1,57 +1,57 @@
 <template>
-    <v-card class="fill-height">
-        <v-card-title>
-          {{warrior.name}}
-        </v-card-title>
-        <v-row class="px-2 d-flex justify-center">
-            <v-card elevation="5" class="d-flex justify-center">
-                <v-img width="200" :src="warrior.profilePic"/>
-            </v-card>
-        </v-row>
-        <v-row v-for="(attribute) in Object.keys(warrior.attributes)" :key="attribute" class="px-2">
-            <v-col cols="5">
-                {{attribute.toUpperCase()}}
-            </v-col>
-            <v-col cols="7">
-                <v-progress-linear class="rounded" :value="warrior.attributes[attribute]" color="blue-grey" height="25">
-                    <template v-slot:default="{ value }">
-                        <strong>{{value}}</strong>
-                    </template>
-                </v-progress-linear>
-            </v-col>
-        </v-row>
-        <v-spacer/>
-        <v-row class="px-2">
-            <v-col cols="12">
-                <v-progress-linear class="rounded" :value="warrior.health" color="green" height="25">
-                    <template v-slot:default="{ value }">
-                        <strong>{{ Math.round(value) }}</strong>
-                    </template>
-                </v-progress-linear>
-            </v-col>
-        </v-row>
-        <v-snackbar>Hello, world!</v-snackbar>
-        <v-card-actions v-if="playable">
-            <v-col cols="6">
-                <v-btn block elevation="2" @click="$emit('attack', warrior, false)">Melee</v-btn>
-            </v-col>
-            <v-col cols="6">
-                <v-btn block elevation="2" @click="$emit('attack', warrior, true)">Ranged</v-btn>
-            </v-col>
-        </v-card-actions>
-        <v-card-actions v-else>
-            <v-col cols="6">
-            </v-col>
-        </v-card-actions>
-    </v-card>
+  <v-card class="fill-height">
+    <v-card-title>
+      {{ warrior.name }}
+    </v-card-title>
+    <v-row class="px-2 d-flex justify-center">
+      <v-card class="d-flex justify-center" elevation="5">
+        <v-img :src="warrior.profilePic" width="200"/>
+      </v-card>
+    </v-row>
+    <v-row v-for="(attribute) in Object.keys(warrior.attributes)" :key="attribute" class="px-2">
+      <v-col cols="5">
+        {{ attribute.toUpperCase() }}
+      </v-col>
+      <v-col cols="7">
+        <v-progress-linear :value="warrior.attributes[attribute]" class="rounded" color="blue-grey" height="25">
+          <template v-slot:default="{ value }">
+            <strong>{{ value }}</strong>
+          </template>
+        </v-progress-linear>
+      </v-col>
+    </v-row>
+    <v-spacer/>
+    <v-row class="px-2">
+      <v-col cols="12">
+        <v-progress-linear :value="warrior.health" class="rounded" color="green" height="25">
+          <template v-slot:default="{ value }">
+            <strong>{{ Math.round(value) }}</strong>
+          </template>
+        </v-progress-linear>
+      </v-col>
+    </v-row>
+    <v-snackbar>Hello, world!</v-snackbar>
+    <v-card-actions v-if="playable">
+      <v-col cols="6">
+        <v-btn block elevation="2" @click="$emit('attack', warrior, false)">Melee</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn block elevation="2" @click="$emit('attack', warrior, true)">Ranged</v-btn>
+      </v-col>
+    </v-card-actions>
+    <v-card-actions v-else>
+      <v-col cols="6">
+      </v-col>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 export default {
-    name: "Warrior",
-    props: {
-      warrior: Object,
-      playable: Boolean,
-    }
+  name: "Warrior",
+  props: {
+    warrior: Object,
+    playable: Boolean,
+  }
 }
 </script>
